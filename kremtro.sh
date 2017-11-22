cc658146d1531e9ce1e3535244e2b0c275b8e1fb
kthordarson@gmail.com <kthordarson@gmail.com>
2017-11-22 16:33:03 +0100

    add man colors

--- kremtro.sh
+++ kremtro.sh
@@ -58,3 +58,19 @@ alias updateapt='sudo apt-get -y upgrade'
 alias apt='sudo apt -y '
 alias apt-get='sudo apt-get -y'
 
+
+# Color man pages
+man() {
+	env \
+		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+		LESS_TERMCAP_md=$(printf "\e[1;31m") \
+		LESS_TERMCAP_me=$(printf "\e[0m") \
+		LESS_TERMCAP_se=$(printf "\e[0m") \
+		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+		LESS_TERMCAP_ue=$(printf "\e[0m") \
+		LESS_TERMCAP_us=$(printf "\e[1;32m") \
+			man "$@"
+}
+
+
+
