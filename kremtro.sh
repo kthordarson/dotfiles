df4f8654e9d2505a76fe546ded65a5e3b0a69717
kthordarson <kthordarson@gmail.com>
2018-04-17 08:00:53 +0200

    added some stuff

--- kremtro.sh
+++ kremtro.sh
@@ -3,10 +3,12 @@
 # keep this file in ~/dotfiles
 # run setupdotfiles.sh to load kremtro.sh from .bashrc
 # sync with git
-# 
+#
 export PATH=$PATH:~/dotfiles
 
 #basic
+alias kremer='vi /home/kth/dotfiles/kremtro.sh && source ~/.bashrc'
+alias rassgat='echo rassgat'
 alias vi='vim'
 alias ll="ls -lah"
 alias llr="ll -tr"
@@ -27,11 +29,11 @@ alias apt-biggest="dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n"
 alias path='echo -e ${PATH//:/\\n}'
 alias mountt='mount | column -t'
 alias nocomment='grep -Ev '\''^(#|$)'\'''
- 
+
 #alias grep='grep --color=auto'
 #alias egrep='egrep --color=auto'
 #alias fgrep='fgrep --color=auto'
- 
+
 #misc
 alias alljobs='for user in $(cut -f1 -d: /etc/passwd); do crontab -u $user -l; done'
 #alias findit="find . -type f -name"
@@ -40,27 +42,30 @@ updateme() {
   (cd ~/dotfiles/ && git pull)
 }
 
-#networking 
+#networking
 alias port1='netstat -tulanp'
 alias port3="watch -n 1 'netstat -Wnepo'"
 #show external ip address
-alias ipad="dig +short myip.opendns.com @resolver1.opendns.com" 
+alias ipad="dig +short myip.opendns.com @resolver1.opendns.com"
 #alias port4='netstat -an | grep ESTABLISHED | awk '{print $5}' | awk -F: '{print (}' | sort | uniq -c | awk '{ printf("%s\t%s\t",[,() ; for (i = 0; i < (; i++) {printf("*")}; print "" }'))])'
- 
+
 ## get top process eating memory
 alias psmem='ps auxf | sort -nr -k 4'
 alias psmem10='ps auxf | sort -nr -k 4 | head -10'
- 
+
 ## get top process eating cpu ##
 alias pscpu='ps auxf | sort -nr -k 3'
 alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
- 
+
 #diskspace
 alias most='du -hsx * | sort -rh | head -10'
 alias usage="du -h --max-depth=1 | sort -rh"
 alias biggest="find . -printf '%s %p\n'| sort -nr | head -20"
 sbs(){ du -b --max-depth 1 | sort -nr | perl -pe 's{([0-9]+)}{sprintf "%.1f%s", (>=2**30? ((/2**30, "G"): (>=2**20? ((/2**20, "M"): (>=2**10? ((/2**10, "K"): ((, "")}e';}
- 
+
+# find stuff
+alias newest="find . -type f -printf '%TY-%Td-%Tm %.8TT %p\n' | sort -rn | head -n 10"
+
 #export TERM=xterm-color
 #export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
 
