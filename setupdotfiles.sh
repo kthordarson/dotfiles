4b9eeb7b34fbd395b7180c8e14d8e4e2b5f921d2
kthordarson@gmail.com <kthordarson@gmail.com>
2020-04-06 13:26:35 +0200

    fix setup

--- setupdotfiles.sh
+++ setupdotfiles.sh
@@ -28,6 +28,23 @@ else
 fi
 source ~/.bashrc
 
-# copy tmux config
-cp .tmux.conf ~/.tmux.conf
+# copy config files
+cp ~/dotfiles/.tmux.conf ~/.tmux.conf
+cp ~/dotfiles/.vimrc ~/.vimrc
+
+# requirements:
+# curl build-essential cmake vim python3-dev go golang npm
+sudo apt install -y build-essential cmake vim python3-dev go golang npm curl python3-pip
+# setup vim plugins
+mkdir ~/.vim/plugged -p
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/undodir -p
+
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ctrlp.vim ~/.vim/plugged
+git clone https://github.com/ycm-core/YouCompleteMe ~/.vim/plugged/
+cd ~/.vim/plugged/YouCompleteMe
+git submodule update --init --recursive
+./install --all
+
+
 
