a4d482f6a8594d2a60c20d165035cd8cec09373b
kth <kth@fiskur.local>
2022-01-25 19:20:17 +0100

    apps2

---
+++ apps2.sh
@@ -0,0 +1,9 @@
+cd ~
+git clone https://github.com/ReFirmLabs/binwalk.git
+cd binwalk
+sudo ./deps.sh
+sudo python ./setup.py install
+sudo -H pip install git+https://github.com/ahupp/python-magic
+sudo -H pip install git+https://github.com/sviehb/jefferson
+sudo apt-get install -y postgresql
+
