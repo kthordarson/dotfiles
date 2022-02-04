aed69c56144276d2e03731ce0c4084af5539386d
kthordarson <kthordarson@gmail.com>
2022-02-04 14:29:43 +0100

    pipup

--- pipupgrade.py
+++ pipupgrade.py
@@ -1,8 +1,14 @@
 import pkg_resources
 from subprocess import call
 
-packages = [dist.project_name for dist in pkg_resources.working_set]
+localpacks = [k for k in pkg_resources.working_set if 'kth' in k.location]
+# packages = [dist.project_name for dist in pkg_resources.working_set]
 # call("pip3 -vvv install --upgrade " + ' '.join(packages), shell=True)
-call("pip install -v --upgrade " + ' '.join(packages), shell=True)
-
+# call("pip install -v --upgrade --user" + ' '.join(packages), shell=True)
+localcount = len(localpacks)
+idx = 0
+for project in localpacks:
+	pack = project.project_name
+	print(f'[local] {idx}/{localcount} update {pack}')
+	call(f'pip install -v --upgrade --user {pack} ', shell=True)
 
