44a587a47b582accff85d6d64b44772cc93e7881
kthordarson <kthordarson@gmail.com>
2022-08-27 20:31:41 +0200

    pynetprocs

---
+++ pyprocs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import psutil
+
+#p = [f'pid:{k.pid} status:{k.status} {psutil.Process(k.pid).name()}' for k in psutil.net_connections(kind='all')]
+#[k for k in p]
+def get_conns():
+	conns = psutil.net_connections(kind='inet')
+	for k in conns:
+		if k.status == 'LISTEN' or k.status =='ESTABLISHED' or k.status == 'CLOSE_WAIT' or k.status == 'NONE':
+			if k.status == 'ESTABLISHED':
+				kraddr = k.raddr.ip
+			else:
+				kraddr = '-'
+			procname = psutil.Process(k.pid).name()
+			if k.pid is None:
+				kpid = '-'
+			else:
+				kpid = k.pid
+			if k.laddr.ip == '::':
+				kladdrip = '0.0.0.0'
+			else:
+				kladdrip = k.laddr.ip
+			print(f'l:{kladdrip:<13}:{k.laddr.port:<5} {procname} pid:{kpid} s:{k.status} {kraddr}')
+
+def get_listeners():
+	conns = psutil.net_connections(kind='inet')
+	return sorted(set([(k.laddr.port, k.pid, k.laddr.ip, psutil.Process(k.pid).name()) for k in conns if k.status=='LISTEN']))
+
+if __name__ == '__main__':
+	l = get_listeners()
+	for k in l:
+		print(f'port: {k}')
