11ae9284bf7304d83b5eb1d31e973039e4887836
kthordarson <kthordarson@gmail.com>
2022-12-26 22:55:39 +0100

    bigfilesglob

--- bigfiles.py
+++ bigfiles.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-import os
+import os, sys
 import time
 import argparse
 from pathlib import Path
+import glob
 from loguru import logger
 from datetime import datetime
 
@@ -38,50 +39,27 @@ def humanbytes(B):
 	elif TB <= B:
 		return '{0:.0f} TB'.format(B / TB)
 
-
-def get_tree(path, filelist):
-	path = Path(path)
-	if path.is_symlink():
-		logger.warning(f'[symlink] skipping {path}')
-	else:
-		for entry in os.scandir(path):
-			if entry.is_dir(follow_symlinks=False) and not entry.is_symlink() and entry.name not in EXCLUDES:
-				try:
-					get_tree(entry.path, filelist)
-					filelist.append(entry)
-				except Exception as e:
-					logger.error(f'[err] {e} entry:{entry} entry.is_symlink():{entry.is_symlink()}')
-			else:
-				try:
-					if entry.is_file and not entry.is_symlink():
-						filelist.append(entry)
-				except FileNotFoundError as e:
-					logger.error(e)
-				# try:
-				#     entry.stat(follow_symlinks=False).st_size
-				# except FileNotFoundError as e:
-				#     logger.warning(f'[err] {e}')
-				# except Exception as e:
-				#     logger.error(f'[err] {e}')
-
 if __name__ == '__main__':
-	myparse = argparse.ArgumentParser(description="Find new files")
-	myparse.add_argument('--path', metavar='path', type=str, help="Path to search", default=".")
+	myparse = argparse.ArgumentParser(description="Find new files", exit_on_error=False)
+	_default = str(Path(myparse.prog).parent)
+	# myparse.add_argument('--path', metavar='path', type=str, help="Path to search", nargs='?', const='.', action='store_const')
+	myparse.add_argument('path', nargs='?', type=str, default=_default,	 metavar='input_path')
 	myparse.add_argument('--maxfiles', metavar='maxfiles', type=int, help="Limit to x results", default=30)
-	myparse.add_argument('--excludes', help="use exclude list", action='store_true', default=False)
+	myparse.add_argument('--excludes', help="use exclude list", action='store_true', default=False)	
 	args = myparse.parse_args()
 	if not args.excludes:
 		EXCLUDES = []
-	input_path = args.path
 	maxfiles = args.maxfiles
+	#input_path = args.path
 	filelist = []
-	get_tree(input_path, filelist)
-	reslist = [(k, k.stat().st_size) for k in filelist if k.is_file()]
-	reslist.sort(key=lambda x: x[1], reverse=False)
-	logger.debug(f'[done] f:{len(filelist)} r:{len(reslist)}')
-	for file in reslist[-maxfiles:]:
-		fitem = Path(file[0])
-		parent = str(fitem.parent)
-		if parent == '.':
-			parent = ''
-		print(f'{humanbytes(file[1]):<5}  file: {parent}/{fitem.name[:30]:<30} ')
+	filelist = [Path(k) for k in glob.glob(str(Path(args.path))+'/**',recursive=True, include_hidden=True)]
+	if filelist:
+		reslist = [(k, k.stat().st_size) for k in filelist if k.is_file()]
+		reslist.sort(key=lambda x: x[1], reverse=False)
+		logger.debug(f'[done] f:{len(filelist)} r:{len(reslist)}')
+		for file in reslist[-maxfiles:]:
+			fitem = Path(file[0])
+			parent = str(fitem.parent)
+			if parent == '.':
+				parent = ''
+			print(f'{humanbytes(file[1]):<5}  file: {parent}/{fitem.name[:30]:<30} ')
