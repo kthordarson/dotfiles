69b2363b4f06162f7a594949021889dea6553995
kthordarson <kthordarson@gmail.com>
2023-03-17 18:35:47 +0100

    morenewfiles

--- newfiles.py
+++ newfiles.py
@@ -7,24 +7,36 @@ from pathlib import Path
 from loguru import logger
 from datetime import datetime
 import glob
+from dataclasses import dataclass, field
+import operator
 
-def xfilelist_generator(path):
-    # foo
-    # filelist_ = [Path(k) for k in glob.glob(str(Path(path))+'/**',recursive=True, include_hidden=True)]
-    filelist_= []
+@dataclass(order=True, frozen=False)
+class FileItem:
+	sort_index: int = field(init=False, repr=False)
+	name: Path
+	size: int
+	st_atime: int
+	st_mtime: int
+	st_ctime: int
+	def __init__ (self, name:Path):
+		self.name = name
+		self.filename = str(name.name)
+		self.size = self.name.stat().st_size
+		self.st_atime = self.name.stat().st_atime
+		self.st_mtime = self.name.stat().st_mtime
+		self.st_ctime = self.name.stat().st_ctime
+	def __str__(self):
+		return f'{self.name}'
+	def get_size(self):
+		return get_size_format(self.size,suffix='B')
 
-    for k in glob.glob(str(Path(path))+'/**',recursive=True, include_hidden=True):
-        try:
-            filelist_.append(Path(k))
-        except KeyboardInterrupt as e:
-            logger.error(e)
-    logger.debug(f'[flg] {len(filelist_)}')
-    for k in filelist_:
-        try:
-            if Path(k).is_file() and not Path(k).is_symlink():
-                yield((Path(k), k.stat().st_ctime))
-        except PermissionError as e:
-            logger.warning(f'[err] {e} k={k}')
+
+def get_size_format(b, factor=1024, suffix="B"):
+	for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+		if b < factor:
+			return f"{b:.2f} {unit}{suffix}"
+		b /= factor
+	return f"{b:.2f} {suffix}"
 
 def filelist_generator(startpath):
 	# foo
@@ -34,7 +46,7 @@ def filelist_generator(startpath):
 	else:
 		startpath += '/**'
 	startpath = Path(startpath)
-	filelist_ = [Path(k) for k in glob.glob(str(startpath),recursive=True, include_hidden=True)]
+	filelist_ = [FileItem(Path(k)) for k in glob.glob(str(startpath),recursive=True, include_hidden=True)]
 	logger.debug(f'[flg] :{len(filelist_)}')
 	for file in filelist_:
 		try:
@@ -43,37 +55,54 @@ def filelist_generator(startpath):
 					if p in EXCLUDES:
 						break
 				else:
-					yield((Path(file), file.stat().st_size))
+					yield((Path(file), file.stat().st_size, file.stat().st_ctime))
 		except PermissionError as e:
 			logger.warning(f'[err] {e} k={file}')
 
 
 
 if __name__ == '__main__':
-    myparse = argparse.ArgumentParser(description="Find new files")
-    _default = str(Path(myparse.prog).parent)
+	myparse = argparse.ArgumentParser(description="Find new files")
+	_default = str(Path(myparse.prog).parent)
 	# myparse.add_argument('--path', metavar='path', type=str, help="Path to search", nargs='?', const='.', action='store_const')
-    myparse.add_argument('path', nargs='?', type=str, default=_default,	 metavar='input_path')
-    #myparse.add_argument('--path', metavar='path', type=str, help="Path to search", default=".")
-    myparse.add_argument('--maxfiles', metavar='maxfiles', type=int, help="Limit to x results", default=30)
-    myparse.add_argument('--reverse','-r', help="reverse", action='store_true', dest='reverse', default=False)
-    myparse.add_argument('--excludes', help="use exclude list", action='store_true', default=False)
-    args = myparse.parse_args()
-    if args.excludes:
-        EXCLUDES = ['.git', '__pycache__', '.idea', '.vscode', '.ipynb_checkpoints']
-    else:
-        EXCLUDES = []
-    maxfiles = args.maxfiles
-    if args.reverse:
-        reverse = True
-    else:
-        reverse = False
-    filelist = []
-    reslist = [k for k in filelist_generator(args.path)]
-    reslist.sort(key=lambda x: x[1], reverse=args.reverse)
-    #logger.debug(f'[done] r:{len(reslist)}')
-    print(f'{"filetime":<27}{"age":<8}{"file":<20}')
-    for file in reslist[-maxfiles:]:
-        filetime = datetime.fromtimestamp(file[0].stat().st_ctime)
-        age = datetime.now() - filetime
-        print(f'{filetime} {age.seconds:<8} {file[0]}')
+	myparse.add_argument('path', nargs='?', type=str, default=_default,	 metavar='input_path')
+	#myparse.add_argument('--path', metavar='path', type=str, help="Path to search", default=".")
+	myparse.add_argument('--maxfiles', '-m', metavar='maxfiles', type=int, help="Limit to x results", default=30)
+	myparse.add_argument('--reverse','-r', help="reverse", action='store_true', dest='reverse', default=False)
+	myparse.add_argument('--excludes', '-e', help="use exclude list", action='store_true', default=False)
+	myparse.add_argument('--sort', '-s', metavar='sort', type=str, help="sort by ctime/mtime/atime", default='ctime')
+	args = myparse.parse_args()
+	if args.excludes:
+		EXCLUDES = ['.git', '__pycache__', '.idea', '.vscode', '.ipynb_checkpoints']
+	else:
+		EXCLUDES = []
+	maxfiles = args.maxfiles
+	if args.reverse:
+		reverse = True
+	else:
+		reverse = False
+	filelist = []
+	#reslist = [k for k in filelist_generator(args.path)]
+	if args.path.endswith('/'):
+		startpath = args.path + '**'
+	else:
+		startpath = args.path + '/**'
+	#filelist = [k for k in filelist_generator(args.path)]
+	filelist = [FileItem(Path(k)) for k in glob.glob(startpath,recursive=True, include_hidden=True)]
+	if args.sort == 'ctime':
+		filelist = sorted(filelist, key=operator.attrgetter('st_ctime'), reverse=args.reverse)
+	if args.sort == 'atime':
+		filelist = sorted(filelist, key=operator.attrgetter('st_mtime'), reverse=args.reverse)
+	if args.sort == 'mtime':
+		filelist = sorted(filelist, key=operator.attrgetter('st_mtime'), reverse=args.reverse)
+
+	#reslist.sort(key=lambda x: x[1], reverse=args.reverse)
+	#logger.debug(f'[done] r:{len(reslist)}')
+	timefmt = '%d-%m-%Y %H:%M:%S'
+	print(f'{"file":<30}{"ctime":<21}{"mtime":<21}{"atime":<21}')
+	print(f'{"-"*90}')
+	for file in filelist[-maxfiles:]:
+		ct = datetime.fromtimestamp(file.st_ctime).strftime(timefmt)
+		mt = datetime.fromtimestamp(file.st_mtime).strftime(timefmt)
+		at = datetime.fromtimestamp(file.st_atime).strftime(timefmt)
+		print(f'{file.filename[:30]:30} | {ct} | {mt} | {at}')
