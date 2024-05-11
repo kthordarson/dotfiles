d3d612c16770d5da4f9dafa7271ad47891418001
kthordarson <kthordarson@gmail.com>
2024-05-11 11:20:48 +0000

    update symlinksfixer update piplist

--- piplist.py
+++ piplist.py
@@ -1,15 +1,118 @@
-import pkg_resources
-from subprocess import call
+import os
+import sys
+import requests
+import json
+import importlib
+import importlib_metadata
+import xmlrpc.client
+from loguru import logger
+import time
+def get_modules():
+	allpacks = [k for k in set([k for k in importlib_metadata.entry_points() ])]
+	usrpacks = [k for k in set([k for k in importlib_metadata.entry_points() if str(k.dist._path).startswith('/usr')])]
+	localpacks = [k for k in set([k for k in importlib_metadata.entry_points() if str(k.dist._path).startswith('/home/')])]
+	return allpacks, usrpacks, localpacks
 
-localpacks = [k for k in pkg_resources.working_set if 'kth' in k.location]
-# packages = [dist.project_name for dist in pkg_resources.working_set]
-# call("pip3 -vvv install --upgrade " + ' '.join(packages), shell=True)
-# call("pip install -v --upgrade --user" + ' '.join(packages), shell=True)
-localcount = len(localpacks)
-idx = 0
-for project in localpacks:
-	pack = project.project_name
-	print(f'[local] {idx}/{localcount} p:{pack}')
-	# call(f'pip install -v --upgrade --user {pack} ', shell=True)
-	idx += 1
+def make_json_link(modulename):
+	info = {'modulename': modulename, 'url': f'https://pypi.org/pypi/{modulename}/json'}
+	return info
 
+def get_pypi_json(module):
+	url = module['url']
+	jsondata = {}
+	try:
+		r = requests.get(url)
+	except Exception as e:
+		logger.error(f'Unhandled Exception: {e} {type(e)} {module=} for {url} for module: {module["modulename"]}')
+		jsondata = {'error': e, 'status_code': r.status_code, 'url': url, 'module': module['modulename']}
+		# Error: <Fault -32500: 'HTTPTooManyRequests: The action could not be performed because there were too many requests by the client.'> <class 'xmlrpc.client.Fault'> https://pypi.org/pypi/pyroute2.ipdb/json
+	if r.status_code == 200:
+		try:
+			jsondata = r.json()
+		except Exception as e:
+			logger.error(f'Unhandled Exception: {e} {type(e)} for {url} response: {r.status_code}')
+			jsondata = {'error': e, 'status_code': r.status_code, 'url': url, 'module': module['modulename']}
+	elif r.status_code == 404:
+		logger.warning(f'modulenotfound {module["modulename"]} {url=} response: {r.status_code} ')
+		jsondata = {'error': '404 Not Found','url': url, 'module': module['modulename']}
+	else:
+		logger.error(f'moduleerror {module["modulename"]} {url} response: {r.status_code}')
+		jsondata = {'error': f'{r.status_code} Error','url': url, 'module': module['modulename']}
+	time.sleep(0.2) # sleep for 200ms for rate limiting
+	return jsondata
+
+def get_installed_version(packname):
+	try:
+		installed_version = importlib_metadata.version(packname)
+	except Exception as e:
+		logger.error(f'Unhandled Exception: {e} {type(e)} for {packname}')
+		installed_version = 'Error'
+	return installed_version
+
+def get_latest_version_online(packname):
+	latest_version = '0.0.0'
+	try:
+		jsoninfo = get_pypi_json(make_json_link(packname))
+	except Exception as e:
+		logger.error(f'Unhandled Exception: {e} {type(e)} for {packname}')
+		latest_version = f'Error {e}'
+	if 'info' in jsoninfo.keys():
+		if 'version' in jsoninfo.get('info').keys():
+			latest_version = jsoninfo.get('info').get('version')
+		else:
+			latest_version = 'Error: No version in info'
+	else:
+		latest_version = 'Error: No info in jsoninfo'
+	return latest_version
+
+def get_latest_version_cache(packname, cachedata):
+	latest_version = '0.0.0'
+	try:
+		jsoninfo = [k for k in cachedata if k.get('info').get('name') == packname][0]
+	except Exception as e:
+		logger.error(f'Unhandled Exception: {e} {type(e)} for {packname}')
+		latest_version = f'Error {e}'
+	if 'info' in jsoninfo.keys():
+		if 'version' in jsoninfo.get('info').keys():
+			latest_version = jsoninfo.get('info').get('version')
+		else:
+			latest_version = 'Error: No version in info'
+	else:
+		latest_version = 'Error: No info in jsoninfo'
+	return latest_version
+
+if __name__ == '__main__':
+	client = xmlrpc.client.ServerProxy('https://pypi.org/pypi')
+	#client.package_releases('roundup')
+	allpacks, usrpacks, localpacks = get_modules()
+	all_installed_names = [k for k in set(k.dist.name for k in allpacks)]
+	usrnames = [k for k in set([k.dist.name for k in usrpacks])]
+	localnames = [k for k in set([k.dist.name for k in localpacks])]
+	dupe_packs = [k for k in usrpacks if k.dist.name in localnames]
+	usrpacklinks = [make_json_link(k) for k in usrnames]
+	lockalpacklinks = [make_json_link(k) for k in localnames]
+	logger.info(f'total: {len(usrnames) + len(localnames)} / {len(usrpacks)+len(localpacks)} allpacks={len(allpacks)} usr: {len(usrpacks)} local: {len(localpacks)} dupe: {len(dupe_packs)} usrpacklinks: {len(usrpacklinks)} localpacklinks: {len(lockalpacklinks)}')
+
+	usrpackinfojson = [get_pypi_json(k) for k in usrpacklinks ]
+	validusrpacks = [k for k in usrpackinfojson if len(k.keys())==5]
+	up_errors = [k for k in usrpackinfojson if 'error' in k.keys()]
+
+	localpackjson = [get_pypi_json(k) for k in lockalpacklinks]
+	lp_errors = [k for k in localpackjson if 'error' in k.keys()]
+	validlocalpackjson = [k for k in localpackjson if len(k.keys())==5]
+
+	logger.info(f'usrpackinfojson: {len(usrpackinfojson)} errors: {len(up_errors)} localpackjson: {len(localpackjson)} errors: {len(lp_errors)}')
+	for p in up_errors:
+		logger.warning(f'usrpack Error: {p}')
+	for p in lp_errors:
+		logger.warning(f'localpack Error: {p}')
+	logger.info(f'checking usrpacks')
+	for pack in usrnames:
+		installed_version = get_installed_version(pack)
+		latest_version = get_latest_version_cache(pack, usrpackinfojson)
+		logger.info(f'Name: {pack} localversion: {installed_version} latestversion: {latest_version}')
+	logger.info(f'checking localpacks')
+	for pack in localnames:
+		installed_version = get_installed_version(pack)
+		latest_version = get_latest_version_cache(pack, localpackjson)
+		logger.info(f'Name: {pack} localversion: {installed_version} latestversion: {latest_version}')
--- symlinksfixer.py
+++ symlinksfixer.py
@@ -53,55 +53,57 @@ if __name__ == '__main__':
 	if args.searchtype == 'a':
 		print(f'{Fore.LIGHTBLUE_EX}Searching {Fore.CYAN}{args.path}{Style.RESET_ALL}')
 		#symlinks = [k for k in os.scandir(args.path) if k.is_symlink()  and os.path.exists(os.path.realpath(k))]
-		symlinks = [k for k in glob(pathname=args.path+'**', recursive=True) if Path(k).is_symlink()]
+		symlinks = [k for k in glob(pathname=args.path+'*', recursive=True) if Path(k).is_symlink()]
 		broken_symlinks = [k for k in os.scandir(args.path) if k.is_symlink()  and not os.path.exists(os.path.realpath(k))]
-	if len(symlinks) == 0:
-		print(f'{Fore.LIGHTBLUE_EX}Found {Fore.CYAN}{len(symlinks)} symlinks in {Fore.CYAN}{args.path}{Style.RESET_ALL}')
-	elif len(symlinks) > 0:
-		print(f'{Fore.LIGHTBLUE_EX}Found {Fore.CYAN}{len(symlinks)} {Fore.LIGHTBLUE_EX}working and {Fore.CYAN}{len(broken_symlinks)} {Fore.LIGHTBLUE_EX}broken symlinks in {Fore.CYAN}{args.path}{Style.RESET_ALL}')
-		if args.findbroken:
-			# attempt to find broken links
-			for idx, sl_ in enumerate(broken_symlinks):
-				sl = Path(sl_)
-				broken = sl.name
-				candidates = [k for k in glob(str(args.path)+'/**',recursive=True) if broken in k and os.path.exists(k)]
-				print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] found {Fore.CYAN}{len(candidates)} {Fore.LIGHTBLUE_EX}candidates for {Fore.CYAN}{sl.name} {Style.RESET_ALL}')
+	print(f'{Fore.LIGHTBLUE_EX}Found {Fore.CYAN}{len(symlinks)} {Fore.LIGHTBLUE_EX}working and {Fore.CYAN}{len(broken_symlinks)} {Fore.LIGHTBLUE_EX}broken symlinks in {Fore.CYAN}{args.path}{Style.RESET_ALL}')
+	if args.findbroken:
+		# attempt to find broken links
+		for idx, sl_ in enumerate(broken_symlinks):
+			sl = Path(sl_)
+			print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] Searching {args.path} for {Fore.LIGHTGREEN_EX}{sl.name} {Style.RESET_ALL}')
+			candidates = [k for k in glob(args.path+'/**',recursive=True) if sl.name in k and os.path.exists(k) and os.path.isfile(k) and not os.path.islink(k) ]
+			spath = str(Path(os.path.realpath(sl)).parent)+'/**'
+			print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] Searching {spath} for {Fore.LIGHTGREEN_EX}{sl.name} {Style.RESET_ALL}')
+			candidates2 = [k for k in glob(spath,recursive=True) if sl.name in k and os.path.exists(k) and os.path.isfile(k) and not os.path.islink(k) ]
+			print(f' found {Fore.CYAN}{len(candidates)}  +  {len(candidates2)} {Fore.LIGHTBLUE_EX}candidates for {Fore.CYAN}{sl} {Style.RESET_ALL}')
+			for c in candidates:
+				print(f'\t{Fore.LIGHTGREEN_EX}{c}{Style.RESET_ALL}')
+	elif args.removebroken:
+		# attempt to remove broken links, log operations to file
+		for idx, sl_ in enumerate(broken_symlinks):
+			sl = Path(sl_)
+			print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] removing {Fore.CYAN}{sl} {Style.RESET_ALL}')
+			logger.debug(f'removing {sl}')
+			try:
+				os.unlink(sl)
+			except Exception as e:
+				logger.error(f'unhandled {e} {type(e)} {sl=}')
+	elif args.fixbroken:
+		# attempt to find and fix broken links, log operations to file
+		for idx, sl_ in enumerate(broken_symlinks):
+			sl = Path(sl_)
+			print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] Searching for {Fore.LIGHTGREEN_EX}{sl} {Style.RESET_ALL}')
+			# candidates = [k for k in glob(str(args.path)+'/**',recursive=True) if broken in k and os.path.exists(k)]
+			candidates = [k for k in glob(str(args.path)+'/**',recursive=True) if sl.name in k and os.path.exists(k) and os.path.isfile(k) and not os.path.islink(k) ]
+			if len(candidates) == 0:
+				print(f'  {Fore.RED}no candidates for {Fore.LIGHTGREEN_EX}{sl} {Style.RESET_ALL}')
+			elif len(candidates) == 1:
+				# todo fix symlink
+				# print(f'  found candidate {Fore.LIGHTGREEN_EX}{candidates[0]} {Fore.LIGHTBLUE_EX}for {Fore.CYAN}{sl} {Style.RESET_ALL}')
+				try:
+					fix_symlink(symlink=sl, target=candidates[0], dryrun=args.dryrun)
+				except Exception as e:
+					logger.error(f'unhandled {e} {type(e)} {sl=} target={candidates[0]}')
+			elif len(candidates) > 1:
+				# todo select best candidate and fix symlink
+				print(f'  found {Fore.CYAN}{len(candidates)} {Fore.LIGHTBLUE_EX}for {Fore.CYAN}{sl} {Style.RESET_ALL}')
 				for c in candidates:
-					print(f'\t{Fore.LIGHTGREEN_EX}{c}{Style.RESET_ALL}')
-		elif args.removebroken:
-			# attempt to remove broken links, log operations to file
-			for idx, sl_ in enumerate(broken_symlinks):
-				sl = Path(sl_)
-				broken = sl.name
-				print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] removing {Fore.CYAN}{sl} {Style.RESET_ALL}')
-				logger.debug(f'removing {sl}')
-				# todo remove broken symlinks...
-		elif args.fixbroken:
-			# attempt to find and fix broken links, log operations to file
-			for idx, sl_ in enumerate(broken_symlinks):
-				sl = Path(sl_)
-				broken = sl.name
-				print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] Searching for {Fore.LIGHTGREEN_EX}{sl} {Style.RESET_ALL}')
-				candidates = [k for k in glob(str(args.path)+'/**',recursive=True) if broken in k and os.path.exists(k)]
-				if len(candidates) == 0:
-					print(f'  {Fore.RED}no candidates for {Fore.LIGHTGREEN_EX}{sl} {Style.RESET_ALL}')
-				elif len(candidates) == 1:
-					# todo fix symlink
-					# print(f'  found candidate {Fore.LIGHTGREEN_EX}{candidates[0]} {Fore.LIGHTBLUE_EX}for {Fore.CYAN}{sl} {Style.RESET_ALL}')
-					try:
-						fix_symlink(symlink=sl, target=candidates[0], dryrun=args.dryrun)
-					except Exception as e:
-						logger.error(f'unhandled {e} {type(e)} {sl=} target={candidates[0]}')
-				elif len(candidates) > 1:
-					# todo select best candidate and fix symlink
-					print(f'  found {Fore.CYAN}{len(candidates)} {Fore.LIGHTBLUE_EX}for {Fore.CYAN}{sl} {Style.RESET_ALL}')
-					for c in candidates:
-						print(f'{Fore.LIGHTGREEN_EX}\t{c}{Style.RESET_ALL}')
-		elif args.showbroken:
-			for idx, sl_ in enumerate(broken_symlinks):
-				sl = Path(sl_)
-				print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] {Fore.BLUE}{sl} -> {Fore.CYAN}{os.path.realpath(sl)} {Style.RESET_ALL}')
-		elif args.showall:
-			for idx, sl_ in enumerate(symlinks):
-				sl = Path(sl_)
-				print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(symlinks)}] {Fore.BLUE}{sl} -> {Fore.CYAN}{os.path.realpath(sl)} {Style.RESET_ALL}')
+					print(f'{Fore.LIGHTGREEN_EX}\t{c}{Style.RESET_ALL}')
+	elif args.showbroken:
+		for idx, sl_ in enumerate(broken_symlinks):
+			sl = Path(sl_)
+			print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(broken_symlinks)}] {Fore.BLUE}{sl} -> {Fore.CYAN}{os.path.realpath(sl)} {Style.RESET_ALL}')
+	elif args.showall:
+		for idx, sl_ in enumerate(symlinks):
+			sl = Path(sl_)
+			print(f'{Fore.LIGHTBLUE_EX}[{idx}/{len(symlinks)}] {Fore.BLUE}{sl} -> {Fore.CYAN}{os.path.realpath(sl)} {Style.RESET_ALL}')
