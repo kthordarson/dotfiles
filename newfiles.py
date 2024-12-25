c175cfd5b5254b7f7af44132ef0357378e81a11f
kthordarson <kthordarson@gmail.com>
2024-12-25 16:18:10 +0000

    newfilesfix

--- newfiles.py
+++ newfiles.py
@@ -10,12 +10,9 @@ import glob
 import operator
 from utils import filelist_generator, get_size_format, FileItem, EXCLUDES
 
-
-
-
 def debugprintlist(filelist):
-	#reslist.sort(key=lambda x: x[1], reverse=args.reverse)
-	#logger.debug(f'[done] r:{len(reslist)}')
+	# reslist.sort(key=lambda x: x[1], reverse=args.reverse)
+	# logger.debug(f'[done] r:{len(reslist)}')
 	timefmt = '%d-%m-%Y %H:%M:%S'
 	print(f'{"file":<30}{"ctime":<21}{"mtime":<21}{"atime":<21}')
 	print(f'{"-"*90}')
@@ -26,8 +23,8 @@ def debugprintlist(filelist):
 		print(f'{file.filename[:30]:30} | {ct} | {mt} | {at}')
 
 def printlist(filelist, args):
-	#reslist.sort(key=lambda x: x[1], reverse=args.reverse)
-	#logger.debug(f'[done] r:{len(reslist)}')
+	# reslist.sort(key=lambda x: x[1], reverse=args.reverse)
+	# logger.debug(f'[done] r:{len(reslist)}')
 	if args.sort == 'ctime':
 		filelist = sorted(filelist, key=operator.attrgetter('st_ctime'), reverse=args.reverse)
 	if args.sort == 'atime':
@@ -40,7 +37,7 @@ def printlist(filelist, args):
 		if len(str(file.name)) > maxlen:
 			maxlen = len(str(file.name))
 	# print(f'{"file":<maxlen}{args.sort:<21}')
-	#print(f'{"-"*90}')
+	# print(f'{"-"*90}')
 	s0 = 'file'.ljust(maxlen)+str(args.sort)
 	print(s0)
 	for file in filelist[-maxfiles:]:
@@ -50,17 +47,20 @@ def printlist(filelist, args):
 			datefield = datetime.fromtimestamp(file.st_mtime).strftime(timefmt)
 		elif args.sort == 'atime':
 			datefield = datetime.fromtimestamp(file.st_atime).strftime(timefmt)
-		s0 = str(file.name).ljust(maxlen)+str(datefield)
-		print(s0)
-		#print(f'{file.name} | {datefield}')
+		s0 = str(str(file.name).ljust(maxlen)+str(datefield))
+		try:
+			print(f'{s0}')
+		except UnicodeEncodeError as e:
+			print(f'error: {e} {file}')
+		# print(f'{file.name} | {datefield}')
 
 if __name__ == '__main__':
 	myparse = argparse.ArgumentParser(description="Find new files")
 	_default = str(Path(myparse.prog).parent)
 	# myparse.add_argument('--path', metavar='path', type=str, help="Path to search", nargs='?', const='.', action='store_const')
-	#myparse.add_argument('path', type=str, default=_default,	 metavar='input_path')
-	myparse.add_argument('path', nargs='?', type=str, default=_default,	 metavar='input_path')
-	#myparse.add_argument('--path', metavar='path', type=str, help="Path to search", default=".")
+	# myparse.add_argument('path', type=str, default=_default,	 metavar='input_path')
+	myparse.add_argument('path', nargs='?', type=str, default=_default, metavar='input_path')
+	# myparse.add_argument('--path', metavar='path', type=str, help="Path to search", default=".")
 	myparse.add_argument('-wc','--wildcard', required=False, metavar='wildcard', type=str, nargs=1, help="search by wildcard", default='*')
 	myparse.add_argument('--maxfiles', '-m', metavar='maxfiles', type=int, help="Limit to x results", default=30)
 	myparse.add_argument('--reverse','-r', help="reverse", action='store_true', dest='reverse', default=False)
@@ -77,8 +77,8 @@ if __name__ == '__main__':
 	else:
 		reverse = False
 	filelist = []
-	#reslist = [k for k in filelist_generator(args.path)]
+	# reslist = [k for k in filelist_generator(args.path)]
 	filelist = [k for k in filelist_generator(args, exclude_list)]
-	#filelist = [FileItem(Path(k)) for k in glob.glob(startpath,recursive=True, include_hidden=True)]
+	# filelist = [FileItem(Path(k)) for k in glob.glob(startpath,recursive=True, include_hidden=True)]
 	printlist(filelist, args)
 
