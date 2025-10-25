5359facbee3cb93c24c00395141305bae77cbd7b
kthordarson <kthordarson@gmail.com>
2025-10-25 14:25:26 +0000

    portsproc

--- portsproc.sh
+++ portsproc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export IFS=$'\n'
 openports="$(sudo lsof -ni -t -sTCP:LISTEN | sort | uniq -u | tail +2)"
-for pidin in $openports
-    do
-        pid=$(echo "$pidin" | tr -d "\0")
-        cmdline=$(cat /proc/"$pid"/cmdline  | tr -d "\0")
-        listenport=$(sudo lsof -nP -p$pid -f -w | grep LISTEN | awk {'print $9'})
-        echo "$listenport PID: $pid cmd: $cmdline "
-    done
+for pidin in $openports; do
+    pid=$(echo "$pidin" | tr -d "\0")
+    if [[ -r "/proc/$pid/cmdline" ]]; then
+        cmdline=$(cat "/proc/$pid/cmdline" | tr -d "\0")
+        listenport=$(sudo lsof -nP -p"$pid" -f -w | grep LISTEN | awk '{print $9}')
+        echo "$listenport PID: $pid cmd: $cmdline"
+    fi
+done
