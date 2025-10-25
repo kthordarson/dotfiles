3a084eb9c5fe4226a60ddb3c168df5824cd29b69
kthordarson <kthordarson@gmail.com>
2025-10-25 13:35:01 +0000

    kremtro

--- kremtro.sh
+++ kremtro.sh
@@ -304,15 +304,10 @@ find_duplicate_files() {
 
     # Temporary file for storing file info
     local temp_file
-    # Then assign the value
-    temp_file=$(mktemp)
-
-    # Check if mktemp succeeded
-    if [ $? -ne 0 ]; then
+    if ! temp_file=$(mktemp); then
         echo "Error: Failed to create temporary file"
         return 1
     fi
-
 
     # Find files, calculate size and MD5, and filter by size
     find "$1" -type f -size +"${min_size}"c -exec ls -l {} \; -exec md5sum {} \; |
